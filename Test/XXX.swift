c30e115fe20e5be22e4ea649a1448a2bec242674
jianhui <78072865+jiajianhui@users.noreply.github.com>
2026-04-07 17:44:29 +0800

    测试

---
+++ Test/XXX.swift
@@ -0,0 +1,18 @@
+//
+//  XXX.swift
+//  Test
+//
+//  Created by 贾建辉 on 2026/4/7.
+//
+
+import SwiftUI
+
+struct XXX: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    XXX()
+}
